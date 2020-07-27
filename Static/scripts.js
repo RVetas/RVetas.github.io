751af7857c87d7ca14dc9c45b792f0153117aaf3
Vitaliy Ramazanov <ram010172@yandex.ru>
2020-07-27 16:31:52 +0300

    js age calculation

---
+++ Static/scripts.js
@@ -0,0 +1,9 @@
+function calculate_age(dob) {
+    var diff_ms = Date.now() - dob.getTime();
+    var age_dt = new Date(diff_ms);
+
+    return Math.abs(age_dt.getUTCFullYear() - 1970);
+}
+
+var element = document.getElementById("ageBlock");
+element.innerHTML = calculate_age(new Date(1998, 1, 22));
